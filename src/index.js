6652ce6cf83ea68f8c202881f82001f493bdb0ee
Garry <gtoborn@gmail.com>
2017-09-09 13:57:17 -0600

    Saturday class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            count: 4,
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+
+    }
+
+    onInputChange(event) {
+        this.setState( {
+            count: event.target.value,
+        });
+        console.log(event.target.value);
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <input type="number" value={this.state.count}
+                        onChange={this.onInputChange}
+                        min="0" />
+                </div>
+                <br />
+                <div className="block">1</div>
+                <div className="block">2</div>
+                <div className="block">3</div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
